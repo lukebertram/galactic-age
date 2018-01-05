24dbe3e4ba8edd8b608d87631b2c529a08c56d27
Luke Bertram <lukebertram@gmail.com>
2018-01-05 11:15:41 -0800

    add minifyScripts and clean tasks to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,10 @@ var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
-
+var uglify = require('gulp-uglify');
+var utilities = require('gulp-util');
+var buildProduction = utilities.env.production;
+var del = require('del');
 
 //GULP TASKS -------------
 
@@ -30,6 +33,16 @@ gulp.task('concatInterface', function(){
  });
 
 //'$ gulp minifyScripts'
-
+  //optimizes app.js file for browsers by removing whitespace/comments
+gulp.task('minifyScripts', ['jsBrowserify'], function() {
+ return gulp.src('build/js/app.js')
+   .pipe(uglify())
+   .pipe(gulp.dest('./build/js'));
+});
 
 //'$ gulp clean'
+  // removes old versions of build/ and tmp/ dirs to avoid buildup of
+  // old/unwanted versions of files
+gulp.task('clean', function(){
+ return del(['build', 'tmp']);
+});
