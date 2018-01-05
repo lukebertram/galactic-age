426f4b2e903b36ea98c7e903218a2223acae8a70
Luke Bertram <lukebertram@gmail.com>
2018-01-05 09:33:58 -0800

    add input-form and output-list to index.html

---
+++ js/galactic-interface.js
@@ -0,0 +1,8 @@
+import {GalacticCalculator} from './../js/galactic-calculator.js';
+
+$(document).ready(function(){
+  $('#age-submission').submit(function(event){
+    event.preventDefault();
+
+  })
+});
