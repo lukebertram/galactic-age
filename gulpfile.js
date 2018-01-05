f31c3b2a774622684d06e0d7fa10d8cf8cfae0d8
Luke Bertram <lukebertram@gmail.com>
2018-01-05 11:10:58 -0800

    add conctInterface task to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
 
 
 //GULP TASKS -------------
@@ -20,7 +21,13 @@ gulp.task('jsBrowserify', ['concatInterface'], function() {
 });
 
 //'$ gulp concatInterface'
-
+  // gathers all interface files from js/ dir, concatenates them, and
+  // stores them in tmp/ dir.
+gulp.task('concatInterface', function(){
+ return gulp.src(['js/*-interface.js'])
+   .pipe(concat('allConcat.js'))
+   .pipe(gulp.dest('./tmp'));
+ });
 
 //'$ gulp minifyScripts'
 
