7d7ff41785cc34fd9e3ea3725f949fde26496041
Luke Bertram <lukebertram@gmail.com>
2018-01-06 13:55:05 -0800

    add secBetween() to calculator and pass spec test

--- js/galactic-calculator.js
+++ js/galactic-calculator.js
@@ -1,14 +1,21 @@
 import moment from 'moment';
 
 export class GalacticCalculator{
-  constructor(){
-
-  }
+  constructor(skin){
+    this.skin = skin || 'hot pink';
+  };
 
   // accepts amount of time in years (integer) and returns converted to seconds
   yearsToSeconds(integer){
     const output = moment.duration(integer, 'years').asSeconds();
     console.log(output)
     return output;
-  }
+  };
+
+  // accept two date strings and calculate the number of seconds between them
+  secBetween(dateA, dateB){
+    const momentA = moment(dateA);
+    const momentB = moment(dateB);
+    return Math.abs(momentA.diff(momentB, 'seconds'));
+  };
 }
